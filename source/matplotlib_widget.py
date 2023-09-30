06672bd4586591d150f27688547db7f10168a9eb
ge47zot <stefan.dan@tum.de>
2023-09-30 22:01:28 +0200

    nothing special

--- source/matplotlib_widget.py
+++ source/matplotlib_widget.py
@@ -9,16 +9,21 @@ class MatplotlibWidget(tk.Frame):
 
         self.figure = Figure(figsize = (width_inch, height_inch), dpi = 100)
         self.ax = self.figure.add_subplot()
+        self.ax.semilogy()
         self.figure.tight_layout()
-
         self.canvas = FigureCanvasTkAgg(self.figure, master = self)
 
-        self.toolbar = NavigationToolbar2Tk(self.canvas,self, pack_toolbar = False)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self, pack_toolbar = False)
         self.toolbar.update()
 
         self.toolbar.pack(side = tk.BOTTOM, fill = tk.X)
         self.canvas.get_tk_widget().pack(side = tk.TOP, fill = tk.BOTH, expand = True)
+        self.configure(padx = 10, pady = 10, relief = 'sunken', borderwidth = 3, bg = 'white')
 
-    def get_ax_and_canvas(self):
-        return self.ax, self.canvas
+    def get_info(self):
+        return self.ax, self.figure, self.canvas
 
+    def clear(self):
+        self.ax.clear()
+        self.ax.semilogy()
+        self.canvas.draw()
