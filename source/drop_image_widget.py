9522921f7eb7eb509686121caa4d4b75e0c57e5b
ge47zot <stefan.dan@tum.de>
2023-09-30 21:56:50 +0200

    support for test images

--- source/drop_image_widget.py
+++ source/drop_image_widget.py
@@ -5,15 +5,22 @@ import tkinterdnd2 as tk2
 class DropImageWidget(tk.Frame):
     def __init__(self, parent, display_widget):
         super().__init__(master = parent)
-        text = tk.Label(self, text = 'drop image here or write path', bg = '#fcba03')
-        text.pack(anchor = tk.NW)
+        tk.Label(self, text = 'Drop an image here or pick a sample image', bg = '#9e9db3',
+                        font = ('Helvetica', 11)).pack(anchor = tk.NW)
         self.display_widget = display_widget
         self.text_var = tk.StringVar()
         self.entrybox = tk.Entry(self, textvariable = self.text_var, width = 80)
         self.entrybox.pack(fill = tk.X)
         self.entrybox.drop_target_register(tk2.DND_FILES)
         self.entrybox.dnd_bind('<<Drop>>', self._drop)
+        self.configure(padx = 10, pady = 10, relief = 'sunken', borderwidth = 3)
 
     def _drop(self, event):
         self.text_var.set(event.data)
+        self.display_widget.display(self.text_var.get())
+
+    def drop_path(self, path):
+        # there is no support for passing data through virtual events in tkinter
+        # the easiest way around is to do a custom function and avoid virtual events
+        self.text_var.set(path)
         self.display_widget.display(self.text_var.get())
